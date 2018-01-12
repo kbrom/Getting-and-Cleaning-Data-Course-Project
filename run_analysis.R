eeb3a12ceded9945f4305fc0063a0666fc1e8c15
Kibrom Haftu <kibromhft@gmail.com>
2018-01-12 18:16:34 +0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -9,9 +9,6 @@
 
 # Load Packages and get the Data
 library(dplyr)
-
-
-
 # download zip file containing data if it hasn't already been downloaded
 zipUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 zipFile <- "UCI HAR Dataset.zip"
@@ -26,20 +23,17 @@ if (!file.exists(dataPath)) {
   unzip(zipFile)
 }
 
-
-
-
-# read training data
+	# read training data
 trainingSubjects <- read.table(file.path(dataPath, "train", "subject_train.txt"))
 trainingValues <- read.table(file.path(dataPath, "train", "X_train.txt"))
 trainingActivity <- read.table(file.path(dataPath, "train", "y_train.txt"))
 
-# read test data
+	# read test data
 testSubjects <- read.table(file.path(dataPath, "test", "subject_test.txt"))
 testValues <- read.table(file.path(dataPath, "test", "X_test.txt"))
 testActivity <- read.table(file.path(dataPath, "test", "y_test.txt"))
 
-# read features, don't convert text labels to factors
+	# read features, don't convert text labels to factors
 features <- read.table(file.path(dataPath, "features.txt"), as.is = TRUE)
 ## note: feature names (in features[, 2]) are not unique
 ##       e.g. fBodyAcc-bandsEnergy()-1,8
@@ -49,15 +43,14 @@ activities <- read.table(file.path(dataPath, "activity_labels.txt"))   # read ac
 colnames(activities) <- c("activityId", "activityLabel")
 
 
-# Step 1 - Merge the training and the test sets to create one data set
-# concatenate individual data tables to make single data table.
-humanActivity <- rbind(
-  cbind(trainingSubjects, trainingValues, trainingActivity),
-  cbind(testSubjects, testValues, testActivity)
-)
+	# Step 1 - Merge the training and the test sets to create one data set
+	# concatenate individual data tables to make single data table.
+	humanActivity <- rbind(
+	  cbind(trainingSubjects, trainingValues, trainingActivity),
+	  cbind(testSubjects, testValues, testActivity)
+	)
 
-
-rm(trainingSubjects, trainingValues, trainingActivity, # remove individual data tables to save memory.
+rm(trainingSubjects, trainingValues, trainingActivity, # You can remove individual data tables to save memory.
    testSubjects, testValues, testActivity)
 
 # assign column names
@@ -68,8 +61,8 @@ colnames(humanActivity) <- c("subject", features[, 2], "activity")
 columnsToKeep <- grepl("subject|activity|mean|std", colnames(humanActivity)) # determine columns of data set to keep based on column name
 
 
-							# ... and keep data in these columns only
-humanActivity <- humanActivity[, columnsToKeep]
+							# ... and keep data in only the following columns. 
+    humanActivity <- humanActivity[, columnsToKeep]
 
 
 # Step 3 - Use descriptive activity names to name the activities in the data set.
@@ -79,18 +72,16 @@ humanActivity$activity <- factor(humanActivity$activity, # replace activity valu
 
                                  levels = activities[, 1], labels = activities[, 2])
 
-
-##############################################################################
 # Step 4 - Appropriately label the data set with descriptive variable names
-##############################################################################
 
-# get column names
-humanActivityCols <- colnames(humanActivity)
 
-# remove special characters
-humanActivityCols <- gsub("[\\(\\)-]", "", humanActivityCols)
+humanActivityCols <- colnames(humanActivity) # get column names
 
-# expand abbreviations and clean up names
+
+
+humanActivityCols <- gsub("[\\(\\)-]", "", humanActivityCols)  # remove special characters
+
+	# expand abbreviations and clean up names
 humanActivityCols <- gsub("^f", "frequencyDomain", humanActivityCols)
 humanActivityCols <- gsub("^t", "timeDomain", humanActivityCols)
 humanActivityCols <- gsub("Acc", "Accelerometer", humanActivityCols)
